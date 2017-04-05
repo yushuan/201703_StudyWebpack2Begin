d058b1310e2338cac8ede8a4fe4564b1ed3e638e
LinYushuan <lin.yushuan@gmail.com>
2017-04-05 16:33:24 +0800

    範例5

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,6 +1,18 @@
-import "../styles/image_viewer.css"
+import "../styles/image_viewer.css";
 
-const image = document.createElement('img');
-image.src = "http://lorempixel.com/400/400";
+import big from "../assets/big.jpg";
+import small from "../assets/small.jpg";
 
-document.body.appendChild(image);
+
+const image1 = document.createElement('img');
+image1.src = "http://lorempixel.com/400/400";
+document.body.appendChild(image1);
+
+
+const image2 = document.createElement('img');
+image2.src = small ;
+document.body.appendChild(image2);
+
+const image3 = document.createElement('img');
+image3.src = big ;
+document.body.appendChild(image3);
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,12 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
+
   entry: './src/index.js',
   output: {
 	  path: path.resolve( __dirname, 'build' ),
-	  filename: 'bundle.js'
+	  filename: 'bundle.js',
+	  publicPath:'build/'
   },
   module: {
      rules: 
@@ -20,6 +22,16 @@ const config = {
 		         loader: 'css-loader'
 		}),
         test: /\.css$/		 
+	   },
+	   {
+		 test: /\.(jpe?g|png|gif|svg)$/,
+		 use: [
+		   	    {
+		         loader: 'url-loader',
+		         options: { limit: 40000 } //40k
+             },
+		 	'image-webpack-loader?bypassOnDebug'
+		 ]
 	   }
      ],
    },
