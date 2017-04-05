9b3ecd3f42b1b52d5b0b38b1830f6ff211463916
LinYushuan <lin.yushuan@gmail.com>
2017-04-05 16:18:48 +0800

    範例2

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
-﻿範例1:
+﻿範例2:
 ------------------------------------------
-免 load 可以 打包 common js 
- 範例
- 
-npm init
-建立 src\ 目錄
-建立 disc\ 或 build\ 目錄
-npm install --save-dev  套件1 套件2
-修改 package.json 加入 run script
+es2015 (es6) -> es5 語法
+npm install --save-dev  babel-loader babel-core babel-preset-env 
 
-npm run  腳本
+新增
 
-預設 node.js 使用 common js 語法
+.babelrc 設定檔
+{
+ "presets": ["babel-preset-env"]
+}
 
+
+webpack.config.js 內
+新增 babel-loader 處理 .js 檔
+
+  module: {
+     rules: [
+	 {
+	    use: 'babel-loader' ,
+               test: /\.js$/		
+	 }
+    ]
+  }
+  
 ------------------------------------------
--- build/bundle.js
+++ build/bundle.js
@@ -68,19 +68,34 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-const sum = (a,b) => a+b;
+"use strict";
 
-module.exports = sum;
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var sum = function sum(a, b) {
+  return a + b;
+};
+
+exports.default = sum;
 
 /***/ }),
 /* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
-const sum = __webpack_require__(0);
+"use strict";
 
-console.log( sum(1,3) );
+
+var _sum = __webpack_require__(0);
+
+var _sum2 = _interopRequireDefault(_sum);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log((0, _sum2.default)(1, 3));
 
 /***/ })
 /******/ ]);
