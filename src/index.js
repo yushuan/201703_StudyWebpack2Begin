03a1c3d4701023210d638828e9e721594a97ee6f
LinYushuan <lin.yushuan@gmail.com>
2017-04-12 15:27:52 +0800

    範例6

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
-import sum from './sum';
-import './image_viewer';
-import '../styles/test.css';
-console.log( sum(1,3) );
+//import sum from './sum';
+//import './image_viewer';
+//import '../styles/test.css';
+//console.log( sum(1,3) );
+//
+
+const button = document.createElement('button');
+button.innerText = 'Click Me';
+button.onclick = () => {
+   //   alert('hello');
+    System.import('./image_viewer').then(
+        module => {
+      //     alert('hello');
+           console.log(module);
+           module.default();
+         }
+    );
+};
+
+document.body.appendChild(button);
